dce56f3904fa9b6fad662f0b018e38a4b765a3c2
Robert <robert.a.argume@gmail.com>
2018-03-12 20:28:41 -0400

    Initial Commit. Add ImageView to MainStory board.

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Robert Argume on 2018-03-12.
+//  Copyright © 2018 Robert Argume. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
